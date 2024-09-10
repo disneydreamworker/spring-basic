01f5892874da23136c776f019440d5dbec51c955
disneydreamworker <cuddlelyholly@naver.com>
2024-09-10 16:37:18 +0900

    cookie

---
+++ src/main/java/com/ssg/springex/web2/controller/TodoReadController.java
@@ -0,0 +1,77 @@
+package com.ssg.springex.web2.controller;
+
+import com.ssg.springex.todo.dto.TodoDTO;
+import com.ssg.springex.web2.service.TodoService;
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "todoReadController", urlPatterns = "/todo/read")
+@Log4j2
+public class TodoReadController extends HttpServlet {
+    private TodoService todoService = TodoService.INSTANCE;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("todo/read.....실행");
+        Long tno = Long.parseLong(req.getParameter("tno"));
+        try {
+            TodoDTO todoDTO = todoService.get(tno);
+
+            // DB로부터 전달받은 글 하나를 req 에다 저장하기 (담기)
+            req.setAttribute("dto", todoDTO);
+
+            //쿠키 찾기
+            Cookie viewTodoCookie = findCookie(req.getCookies(), "viewTodos");
+            String todoListStr = viewTodoCookie.getValue();
+            boolean exist = true;
+
+            if (todoListStr != null && todoListStr.indexOf(tno + "-") >= 0) {
+                exist = true;
+            }
+
+            log.info("exist" + exist);
+
+            if (!exist) {
+                todoListStr += tno + "-";
+                viewTodoCookie.setValue(todoListStr);
+                viewTodoCookie.setMaxAge(60 * 60);
+                viewTodoCookie.setPath("/");
+                resp.addCookie(viewTodoCookie);
+
+            }
+
+            req.getRequestDispatcher("/todo/read.jsp").forward(req, resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ServletException("read page error");
+        }
+    }
+
+    private Cookie findCookie(Cookie[] cookies, String cookieName) {
+
+        Cookie targetCookie = null;
+        if (cookies != null && cookies.length > 0) {
+
+            for (Cookie ck : cookies) {
+                if (ck.getName().equals(cookieName)) {
+                    targetCookie = ck;
+                    break;
+                }
+            }
+        }
+        if (targetCookie == null) {
+            targetCookie = new Cookie(cookieName, "");
+            targetCookie.setPath("/");
+            targetCookie.setMaxAge(60 * 60 * 1);
+
+        }
+        return targetCookie;
+    }
+}
---
+++ src/main/java/com/ssg/springex/web2/service/TodoService.java
@@ -0,0 +1,80 @@
+package com.ssg.springex.web2.service;
+
+import com.ssg.springex.todo.dao.TodoDao;
+import com.ssg.springex.todo.domain.TodoVO;
+import com.ssg.springex.todo.dto.TodoDTO;
+import com.ssg.springex.todo.util.ModelUtil;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log4j2
+public enum TodoService {
+    //INSTANCE; //객체 한개만 만들고 사용할게~ 싱글톤 패턴을 적용한거야~~
+    //여러 개의 컨트롤러는 하나의 객체를 통해서만 서비스 이용 가능. -> 관리하기 편함
+
+    INSTANCE;
+
+    private TodoDao dao;
+    private ModelMapper modelMapper;
+
+    //생성자 인젝션을 통해서 DAO를 인젝션 한다
+
+    TodoService() {
+        dao = new TodoDao();    //직접 주입
+        modelMapper = ModelUtil.INSTANCE.get();
+    }
+
+    public void register(TodoDTO dto) throws Exception {
+        TodoVO vo = modelMapper.map(dto, TodoVO.class); //dto 객체를 vo에 담을거야!
+        //LOG 추가
+        // System.out.println("todo vo : " + vo);
+        log.info(vo);
+
+        dao.insert(vo);
+    }
+
+    public List<TodoDTO> listAll() throws Exception {//등록된 글 목록 반환하는 기능
+
+        List<TodoVO> voList = dao.selectAllList();
+        log.info(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
+        log.info(voList);
+
+        List<TodoDTO> dtoList = voList.stream().map(vo->modelMapper.map(vo, TodoDTO.class)).collect(Collectors.toList());
+
+        return dtoList;
+    }
+
+/*
+    public void register(TodoDTO dto) { //글 하나를 등록하는 기능
+
+    }
+
+    public List<TodoDTO> getList(){ //등록된 글 목록 반환하는 기능
+
+        //10개의 글을 만들어서 글 전체의 리스트 객체를 반환함
+        List<TodoDTO> todoDTOS = IntStream.range(0,10).mapToObj(i->{
+            TodoDTO dto = new TodoDTO();
+            dto.setTno((long)i);
+            dto.setTitle("todo..." + i);
+            dto.setDueDate(LocalDate.now());
+            return dto;
+        }).collect(Collectors.toList());
+
+        return todoDTOS;
+    }
+*/
+
+    public TodoDTO get(Long tno) {
+        TodoDTO dto = new TodoDTO();
+        dto.setTno(tno);
+        dto.setTitle("sample todo");
+        dto.setDueDate(LocalDate.now());
+        dto.setFinished(true);
+        return dto;
+    }
+
+}
